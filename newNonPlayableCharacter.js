c233032425f757b1d47a3c3721b8b25ac9c12492
princessxj9 <johnsonprincess25@gmail.com>
2022-04-16 11:04:00 -0400

    commit

--- newNonPlayableCharacter.js
+++ newNonPlayableCharacter.js
@@ -23,10 +23,13 @@ function newNonPlayableCharacter(x, y) {
 
     setInterval(moveCharacter, 1)
 
-    function walkEast() {
+    async function walkEast(time) {
         direction = 'east'
         element.src = `./assets/red-character/east.gif`
+        await sleep(time)
+        stop()
     }
+   
 
     function walkNorth() {
         direction = 'north'
@@ -56,4 +59,10 @@ function newNonPlayableCharacter(x, y) {
         walkSouth: walkSouth,
         stop: stop
     }
+}
+
+function sleep(time){
+    return new Promise(resolve => {
+        setTimeout(resolve, time)
+    })  
 }
